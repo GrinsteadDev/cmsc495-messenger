f0eb52d8f69381241402eecdb02888a672447e24
Devin <devin.grinstead@grinsteaddev.com>
2024-01-11 11:55:11 -0500

    Added Auto Pull for git

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask, request
+from git import Repo
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/git-push/")
+@app.route("/git-push/", methods=['GET', 'POST'])
 def update_repository():
-    '''
-    '''
-    
+    repo = Repo(os.path.dirname(os.path.realpath(__file__)))
+    repo.remotes.orgin.pull()
 
 @app.route("/<string:name>/")
 def say_hello(name):
     return f"Hello {name}!"
-
 
 if __name__ == "__main__":
     app.run()
