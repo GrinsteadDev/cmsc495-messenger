eb0e9a4c65a2e3c970ac17b941d5e2be6de88d9e
Devin <devin.grinstead@grinsteaddev.com>
2024-01-11 11:32:54 -0500

    Updates

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/git-push/")
+def update_repository():
+    '''
+    '''
+    
 
 @app.route("/<string:name>/")
 def say_hello(name):
