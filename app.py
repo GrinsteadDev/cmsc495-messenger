93cc86620a490cf9cd5edf4d68fb8ebb6b8a77bf
Devin <devin.grinstead@grinsteaddev.com>
2024-01-11 09:41:15 -0500

    app.py created

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/<string:name>/")
+def say_hello(name):
+    return f"Hello {name}!"
+
+
+if __name__ == "__main__":
+    app.run()
