60f470ac6603d8a44a80e91b7159feb4fc3a6dc3
Devin <devin.grinstead@grinsteaddev.com>
2024-02-02 12:30:54 -0500

    Converted Phillips static pages into python flask templates

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 import os
-from flask import Flask, request
+from flask import Flask, render_template, request
 from git import Repo
+import jinja2
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/<string:name>", defaults={"ext":""})
+@app.route("/<string:name>.<string:ext>")
+def fetch_templates(name, ext):
+    # Attemps to fetch a template
+    if ext in ["", "html", "htm"]:
+        try:
+            return render_template(f"{name}.html")
+        except jinja2.exceptions.TemplateNotFound as e:
+            pass
+    
+    return "", 404
 
 @app.route("/git-push/", methods=['GET', 'POST'])
 def update_repository():
@@ -29,6 +42,8 @@ def update_repository():
 @app.route("/<string:name>/")
 def say_hello(name):
     return f"Hello {name}!<br>Nice Name!<br> Testing Change<br> New Changes"
+
+
 
 if __name__ == "__main__":
     app.run()
