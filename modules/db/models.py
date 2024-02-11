949fc74aa62bf1c3cdb43a72895079d3276586dd
sancdd01 <92106462+sancdd01@users.noreply.github.com>
2024-02-10 18:32:58 -0800

    added docstring, removed unique contraints

--- modules/db/models.py
+++ modules/db/models.py
@@ -2,13 +2,14 @@ from db.db import db
 from sqlalchemy.sql import func
 
 class UserAccount(db.Model):
+    """Represents a user account entity in database"""
     __tablename__ = 'user_account'
 
     id =db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    first_name = db.Column(db.String(50), unique=True, nullable=False)
-    last_name = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50),  nullable=False)
     password = db.Column(db.String(255), unique=True, nullable=False)
     registered_date = db.Column(db.DateTime(timezone=True), default=func.now)
     last_login = db.Column(db.DateTime(timezone=True))
